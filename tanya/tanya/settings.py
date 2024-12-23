d26fc3d46036d9bbf1dafceb94932002b897b2cd
Nastya <nastysha7069@yandex.ru>
2024-12-23 20:48:39 +0300

    v2

--- tanya/tanya/settings.py
+++ tanya/tanya/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-wp&@*k*#zeq^fs4p+e*=6%fbqu-3oc2pfh*(tg^-qvf-3qf!0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
