a0e54dd06dfb17ceaba0adaf65c1b3c29751f5f6
Nastya <nastysha7069@yandex.ru>
2024-12-23 21:07:36 +0300

    v3

--- tanya/tanya/settings.py
+++ tanya/tanya/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wp&@*k*#zeq^fs4p+e*=6%fbqu-3oc2pfh*(tg^-qvf-3qf!0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
